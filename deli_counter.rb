8ea97150696608e43994aeccbde17df61248ed0e
Anthony Chang <awchang12@gmail.com>
2018-05-04 18:10:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,8 @@ def take_a_number(arr, name)
   arr.push(name)
   puts "Welcome, #{name}. You are number #{arr.length} in line."
 end
+
+def now_serving(arr) {
+  puts "Currently serving #{arr.unshift()}."
+end
+}
